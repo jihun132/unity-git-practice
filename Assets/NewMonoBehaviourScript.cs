7317da503bf6c65e06161dd55138c9f694f025dd
ChangMin1 <95290698+ChangMin1@users.noreply.github.com>
2026-09-16 17:48:44 +0900

    스크립트

---
+++ Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    int a;
+    void Start()
+    {
+        a = 1;
+        Debug.Log(a);
+        // Debug.Log("hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
